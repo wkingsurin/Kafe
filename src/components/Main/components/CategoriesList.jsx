b2c1d09790878246512fe9d19ad4af0a05f157d2
Daniil Surin <wkingsurin@gmail.com>
2026-02-26 18:50:54 +0500

    feat: extract and make controlled Category component

--- src/components/Main/components/CategoriesList.jsx
+++ src/components/Main/components/CategoriesList.jsx
@@ -1,64 +1,18 @@
 import classes from "./CategoriesList.module.css";
 
+import Category from "./Category";
+
+import data from "../../../data.json";
+
 export default function CategoriesList() {
+	const tempCategories = data.map((c) => c.category);
+
 	return (
 		<div className={classes.categories}>
 			<ul className={classes.categoriesList}>
-				<li className={classes.category}>
-					<a href="#combo" className={classes.categoryLink}>
-						Комбо-наборы
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#mangal" className={classes.categoryLink}>
-						Блюда с мангала
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#sets" className={classes.categoryLink}>
-						Наборы
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#pizza" className={classes.categoryLink}>
-						Хачапури / Пицца / Лаваш
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#child" className={classes.categoryLink}>
-						Детское меню
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#garnish" className={classes.categoryLink}>
-						Гарниры
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#soups" className={classes.categoryLink}>
-						Супы
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#salads" className={classes.categoryLink}>
-						Салаты
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#desserts" className={classes.categoryLink}>
-						Десерты
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#drinks" className={classes.categoryLink}>
-						Напитки
-					</a>
-				</li>
-				<li className={classes.category}>
-					<a href="#sauces" className={classes.categoryLink}>
-						Соусы
-					</a>
-				</li>
+				{tempCategories.map((category) => (
+					<Category>{category}</Category>
+				))}
 			</ul>
 		</div>
 	);
---
+++ src/components/Main/components/Category.jsx
@@ -0,0 +1,11 @@
+import classes from "./Category.module.css";
+
+export default function Category({ id = "#combo", children }) {
+	return (
+		<li className={classes.category}>
+			<a href={id} className={classes.categoryLink}>
+				{children}
+			</a>
+		</li>
+	);
+}
