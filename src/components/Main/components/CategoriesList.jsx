1e0363b1844f06d3a7ab0d55bdd229031129ba3c
Daniil Surin <wkingsurin@gmail.com>
2026-03-01 18:01:43 +0500

    feat: improve CategoriesList component
    - Added options prop for render different list items in component

--- src/components/Main/components/CategoriesList.jsx
+++ src/components/Main/components/CategoriesList.jsx
@@ -4,18 +4,33 @@ import Category from "./Category";
 
 import data from "../../../data.json";
 
-export default function CategoriesList() {
-	const tempCategories = data.map((c) => ({ name: c.category, id: c.id }));
+export default function CategoriesList({ options = {} }) {
+	const { type = "default", list } = options;
 
-	return (
-		<div className={classes.categories}>
-			<ul className={classes.categoriesList}>
-				{tempCategories.map((category) => (
-					<Category key={category.name} id={category.id}>
-						{category.name}
-					</Category>
-				))}
-			</ul>
-		</div>
-	);
+	if (type === "default") {
+		const tempCategories = data.map((c) => ({ name: c.category, id: c.id }));
+
+		return (
+			<div className={classes.categories}>
+				<ul className={classes.categoriesList}>
+					{tempCategories.map((category) => (
+						<Category key={category.name} id={category.id}>
+							{category.name}
+						</Category>
+					))}
+				</ul>
+			</div>
+		);
+	}
+	if (type === "profile") {
+		return (
+			<div className={classes.categories} style={{justifyContent: 'flex-start'}}>
+				<ul className={classes.categoriesList}>
+					{list.map((l) => (
+						<Category key={l}>{l}</Category>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
