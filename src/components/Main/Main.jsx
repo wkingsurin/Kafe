5372201807537458a95df6c3f1aa9d80884e4f43
Daniil Surin <wkingsurin@gmail.com>
2026-03-01 18:04:33 +0500

    feat: add condition render content in Main
    - Added state to condition render: main / profile
    - Imported components: CategoriesList, Profile

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,20 @@
 import classes from "./Main.module.css";
 
+import { useState } from "react";
+
 import HeroSection from "./components/HeroSection";
 import ContentSection from "./components/ContentSection";
 
 import data from "../../data.json";
+import CategoriesList from "./components/CategoriesList";
+import Profile from "./components/Profile";
 
 export default function Main() {
+	const [mainTab, setMainTab] = useState("profile");
 	const dishesData = data;
 
-	return (
-		<main className={classes.main}>
+	const main = (
+		<>
 			<HeroSection></HeroSection>
 			{dishesData.map((category) => (
 				<ContentSection
@@ -18,6 +23,25 @@ export default function Main() {
 					category={category}
 				></ContentSection>
 			))}
+		</>
+	);
+	const profile = (
+		<>
+			<section className={classes.categoryList}>
+				<div className="container">
+					<CategoriesList
+						options={{ type: "profile", list: ["Главная", "Профиль"] }}
+					></CategoriesList>
+				</div>
+			</section>
+			<Profile></Profile>
+		</>
+	);
+
+	return (
+		<main className={classes.main}>
+			{mainTab === "main" && main}
+			{mainTab === "profile" && profile}
 		</main>
 	);
 }
