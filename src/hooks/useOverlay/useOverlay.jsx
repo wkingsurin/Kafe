371de4941a22aa02678926869b7ce5e11a07b244
Daniil Surin <wkingsurin@gmail.com>
2026-03-03 20:51:54 +0500

    feat: create handlers into custom hook useOverlay
    - Created handlers: setContent, clearContent
    - Improved handler onShowOverlay logic: added setContent
    - Improved handler onHideOverlay logic: added clearContent

--- src/hooks/useOverlay/useOverlay.jsx
+++ src/hooks/useOverlay/useOverlay.jsx
@@ -4,13 +4,15 @@ import { preventScrollJump } from "../../utils";
 export default function useOverlay(initialValue) {
 	const [state, setState] = useState(initialValue);
 
-	const onShowOverlay = () => {
+	const onShowOverlay = (content) => {
 		preventScrollJump("add");
+		setContent(content);
 		openOverlay();
 	};
 
 	const onHideOverlay = () => {
 		preventScrollJump("remove");
+		clearContent();
 		closeOverlay();
 	};
 
@@ -19,6 +21,13 @@ export default function useOverlay(initialValue) {
 	};
 	const closeOverlay = () => {
 		setState((o) => ({ ...o, open: false }));
+	};
+
+	const setContent = (content) => {
+		setState((o) => ({ ...o, content: content }));
+	};
+	const clearContent = () => {
+		setState((o) => ({ ...o, content: null }));
 	};
 
 	return { overlay: state, onShowOverlay, onHideOverlay };
